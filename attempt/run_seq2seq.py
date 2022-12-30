444b3cc4aa52e24c28cf46e8b232e9f494fb9456
puraminy <pouramini@gmail.com>
2022-12-30 18:10:37 +0330

    commit

--- attempt/run_seq2seq.py
+++ attempt/run_seq2seq.py
@@ -113,18 +113,20 @@ def main(dpy, model_path, config_file):
             json_file=config_file)
     else:
         model_args, data_args, training_args, adapter_args = parser.parse_args_into_dataclasses()
+
     # Detecting last checkpoint.
     last_checkpoint = None
     if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
         last_checkpoint = get_last_checkpoint(training_args.output_dir)
         print("#### last_checkpoint ", last_checkpoint)
-        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+        if training_args.resume_from_checkpoint is None or (last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0):
             '''
             raise ValueError(
                 f"Output directory ({training_args.output_dir}) already exists and is not empty. "
                 "Use --overwrite_output_dir to overcome."
             )
             '''
+            last_checkpoint = None
             out = training_args.output_dir
             out += "_" + now
             Path(out).mkdir(parents = True, exist_ok=True)
