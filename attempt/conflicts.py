8777096b31e428186e0ef5ea5edcee9e8ae05465
puraminy <pouramini@gmail.com>
2023-01-01 23:40:33 +0330

    commit

--- attempt/conflicts.py
+++ attempt/conflicts.py
@@ -3,7 +3,7 @@
 def check_conflicts(model_args, data_args, training_args, adapter_args):
     if adapter_args.prompt_tuning:
         assert model_args.use_optimizer, "Prompt tuning uses optimizer" 
-        assert not model_args.prefix_tuning, "Prompt tuning and prefix tuning can't be both on" 
+        assert not adapter_args.prefix_tuning, "Prompt tuning and prefix tuning can't be both on" 
     else:
         if model_args.attn_learning_rate is not None:
             assert model_args.use_optimizer, "Attention learning uses optimizer" 
