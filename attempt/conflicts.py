e9a28b4e8e25d7290199885ae98abb42d741ac2f
puraminy <pouramini@gmail.com>
2023-01-05 19:26:40 +0330

    commit

--- attempt/conflicts.py
+++ attempt/conflicts.py
@@ -2,12 +2,14 @@
 ####### check conflicts of options
 def check_conflicts(model_args, data_args, training_args, adapter_args, kwargs):
     n_tasks = len(data_args.task_name)
+    method = kwargs.setdefault("method", [])
     trainer_shuffle = kwargs.setdefault("trainer_shuffle", False)
     if not adapter_args.prompt_tuning:
         assert "sup" in data_args.template, "The template must be one of sup or unsup methods"
     if n_tasks > 1:
         assert not trainer_shuffle, "Trainer can't be shuffled for multi-task. The data is interleaved"
     if adapter_args.prompt_tuning:
+        assert "pt" in method, "Prompt tuning is not in the selected methods"
         assert kwargs.use_optimizer, "Prompt tuning uses optimizer" 
         assert training_args.learning_rate > 0.01, "Learning rate is too small for prompt tuning"
         assert not adapter_args.prefix_tuning, "Prompt tuning and prefix tuning can't be both on" 
@@ -17,6 +19,8 @@ def check_conflicts(model_args, data_args, training_args, adapter_args, kwargs):
         else:
             assert not kwargs.use_optimizer, "No need to use optimizer" 
         if adapter_args.prefix_tuning:
+            assert "prt" in method, "prefix tuning is not in the selected methods"
             assert training_args.learning_rate > 0.01, "Learning rate is too small for prefix tuning"
         else:
             assert training_args.learning_rate < 0.01, "Learning rate is too high for fine tuning"
+            assert "ft" in method, "fine tuning is not in the selected methods"
