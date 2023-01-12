dd170af216fffc6914a15f7651fef484eb0af7e4
Ahmad <pouramini@gmail.com>
2023-01-12 12:24:07 +0330

    commit

--- attempt/conflicts.py
+++ attempt/conflicts.py
@@ -8,12 +8,14 @@ def check_conflicts(model_args, data_args, training_args, adapter_args, kwargs):
 
     if adapter_args.prefix_tuning:
         assert not adapter_args.prompt_tuning, "Prompt tuning and prefix tuning can't be on at the same time"
-        if model_args.attn_learning_rate is not None:
-            assert kwargs.use_optimizer, "Attention learning uses optimizer" 
-        else:
-            assert not kwargs.use_optimizer, "No need to use optimizer" 
-        assert training_args.learning_rate > 0.01, "Learning rate is too small for prefix tuning"
+        if training_args.do_train:
+            if model_args.attn_learning_rate is not None:
+                assert kwargs.use_optimizer, "Attention learning uses optimizer" 
+            else:
+                assert not kwargs.use_optimizer, "No need to use optimizer" 
+            assert training_args.learning_rate > 0.01, "Learning rate is too small for prefix tuning"
     elif not adapter_args.prompt_tuning:
         assert not adapter_args.prefix_tuning, "Prompt tuning and prefix tuning can't be on at the same time"
-        assert training_args.learning_rate < 0.01, "Learning rate is too high for fine tuning"
-        assert kwargs.opt_type == "regular", "Fine tuning needs regular optimizer"
+        if training_args.do_train:
+            assert training_args.learning_rate < 0.01, "Learning rate is too high for fine tuning"
+            assert kwargs.opt_type == "regular", "Fine tuning needs regular optimizer"
