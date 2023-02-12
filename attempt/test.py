90546b40fc5adfb05229a3650564f290708c4490
puraminy <pouramini@gmail.com>
2023-02-12 17:42:12 +0330

    commit

--- attempt/test.py
+++ attempt/test.py
@@ -7,3 +7,12 @@ def an(temperature, step, router, amin=1., arate=0.001):
             logits=router).rsample()            
      return router, temperature 
 
+class A():
+    anneal_min = 0.001
+    anneal_dir = -1
+
+    def anneal(self, i_step):
+         t = max(self.anneal_min,
+             self.router_temperature * np.exp(self.anneal_dir * self.anneal_rate * i_step))
+         self.router_temperature = t
+
