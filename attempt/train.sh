4a1bae1cb89e6802ff5b3f315c2ee5454ab7dea5
puraminy <pouramini@gmail.com>
2023-02-12 17:44:33 +0330

    commit

--- attempt/train.sh
+++ attempt/train.sh
@@ -248,7 +248,7 @@ if [ "$method" = "ptat" ]; then
 	params="${params} --@^attn_learning_rate=0.01#0.001"
 	params="${params} --@attn_method=rb#!sub#!linear"
 	params="${params} --@anneal_dir=-0.001#-1"
-	params="${params} --@^router_temperature=4."
+	params="${params} --@router_temperature=4."
 	params="${params} --anneal_min=0.001"
 	params="${params} --anneal_rate=none"
 	params="${params} --@num_source_prompts=8#8"
