f1d1e6f9db8178c384ab50432e34ef821d14c339
puraminy <pouramini@gmail.com>
2023-02-15 21:13:54 +0330

    commit

--- attempt/metrics/metrics.py
+++ attempt/metrics/metrics.py
@@ -39,7 +39,7 @@ TASK_TO_METRICS = {
                    "xReact": ["rouge"],
                    "mrpc": ["accuracy", "f1"],
                    "cola": ['matthews_correlation'],
-                   "stsb": ['pearson_corrcoef', 'spearmanr'],
+                   "stsb": ['pearson_corrcoef', 'spearman_corrcoef'],
                    'sst2': ['accuracy'],
                    "mnli": ["accuracy"],
                    "mnli_mismatched": ["accuracy"],
