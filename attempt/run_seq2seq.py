8a1e588f0e1e2b139e7b0374deb0ca175e6c3938
Ahmad <pouramini@gmail.com>
2023-03-20 14:33:14 +0330

    commit

--- attempt/run_seq2seq.py
+++ attempt/run_seq2seq.py
@@ -1390,6 +1390,9 @@ def train(**kwargs):
             for rm, mask in combs.items():
                 img_list = []
                 for route_method in grm: 
+                    attend_num =len(model.encoder.prompt_encoders) + 1 # one for input
+                    model.encoder.attn_scores = torch.zeros(
+                        (attend_num, attend_num), device=device) 
                     for idx, (task, test_dataset) in enumerate(test_datasets.items()):
                         gen_conf["route_method"] = route_method
                         if mask is not None: 
