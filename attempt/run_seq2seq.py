86bffebf09fb2d2fb8adcc1b24092e598a45c788
Ahmad <pouramini@gmail.com>
2023-04-05 16:13:37 +0330

    commit

--- attempt/run_seq2seq.py
+++ attempt/run_seq2seq.py
@@ -830,6 +830,8 @@ def train(**kwargs):
     mylogs.bp("penc")
     prompts_prefix = kwargs.setdefault("prompts_prefix", "") 
     if prompts_prefix is None: prompts_prefix = ""
+    if prompts_prefix in kwargs:
+        prompts_prefix = kwargs[prompts_prefix]
     prompts_prefix = prompts_prefix + "_" + str(kwargs.expid)
     if prompts_prefix and training_args.do_train:
         prompts_dir = model_args.prompt_encoders_dir
