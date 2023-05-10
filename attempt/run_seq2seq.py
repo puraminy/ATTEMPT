cf23c741fe6da020621b95283097c2e09ed20111
Ahmad <pouramini@gmail.com>
2023-05-10 14:44:21 +0330

    commit

--- attempt/run_seq2seq.py
+++ attempt/run_seq2seq.py
@@ -940,7 +940,8 @@ def train(**kwargs):
                 encoder.is_shared = False
             if kwargs.setdefault("init_from_words", False):
                 encoder.init_embs_from_words(model.get_input_embeddings())
-            if load_source_prompts: # and not "_com" in prompt and not "_for" in prompt:
+            if load_source_prompts and not "_for" in prompt: 
+                # and not "_com" in prompt and not "_for" in prompt:
                 if bp == "load":
                     breakpoint()
                 is_loaded = encoder.load(prompts_dir, prefix=prompts_prefix,
