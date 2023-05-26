3af1c3510aff3e844ee1748a59b031dd876bb7cb
Ahmad <pouramini@gmail.com>
2023-05-26 08:23:47 +0330

    commit

--- attempt/mylogs.py
+++ attempt/mylogs.py
@@ -5,6 +5,10 @@ from pytz import timezone
 import datetime
 from pathlib import Path
 from deepdiff import DeepDiff
+import pickle
+from pathlib import Path
+from appdirs import *
+
 main_args = {}
 prev_args = {}
 prev_main_vars = {}
@@ -169,3 +173,44 @@ def set_args(args):
     tlog.setLevel(logging.INFO)
 
 
+
+def save_obj(obj, name, directory, data_dir=True, common=False):
+    if obj is None or name.strip() == "":
+        logging.info(f"Empty object to save: {name}")
+        return
+    if not data_dir or name.startswith("chk_def_"):
+        folder = directory
+    elif common:
+        folder = user_data_dir(appname, appauthor) + "/" + directory  
+    else:
+        folder = user_data_dir(appname, appauthor) + "/profiles/" + profile + "/" + directory
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    fname = os.path.join(folder, name + '.pkl')
+    with open(fname, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name, directory, default=None, data_dir=True, common =False):
+    if not data_dir:
+        folder = directory
+    elif common:
+        folder = user_data_dir(appname, appauthor) + "/" + directory  
+    else:
+        folder = user_data_dir(appname, appauthor) + "/profiles/" + profile + "/" + directory
+
+    fname = os.path.join(folder, name + ".pkl")
+    obj_file = Path(fname)
+    if not obj_file.is_file():
+        return default
+    with open(fname, 'rb') as f:
+        return pickle.load(f)
+
+
+def is_obj(name, directory, common = False):
+    if common:
+        folder = user_data_dir(appname, appauthor) + "/" + directory  
+    else:
+        folder = user_data_dir(appname, appauthor) + "/profiles/" + profile + "/" + directory
+    if not name.endswith('.pkl'):
+        name = name + '.pkl'
+    fname = os.path.join(folder, name)
