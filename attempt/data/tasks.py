06dd169c97518af262e23afab1f16583f0a55464
Ahmad <pouramini@gmail.com>
2023-06-04 17:19:28 +0330

    commit

--- attempt/data/tasks.py
+++ attempt/data/tasks.py
@@ -487,7 +487,7 @@ class PIQA(AbstractTask):
     split_to_data_split = {"train": "train",
                            "validation": "validation",
                            "test": "validation"}
-    map_labels = {"0.0":"Choice1", "1.0":"Choice2"}
+    map_labels = {"0":"Choice1", "1":"Choice2", "0.0":"Choice1", "1.0":"Choice2"}
 
     def load_dataset(self, split):
         # return datasets.load_dataset('piqa', split=split)
