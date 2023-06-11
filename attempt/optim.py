f9eda3076f3b169c79ec30e07f5c9926931f741a
Ahmad <pouramini@gmail.com>
2023-06-11 22:08:08 +0330

    commit

--- attempt/optim.py
+++ attempt/optim.py
@@ -48,6 +48,9 @@ class Scheduler:
             self.schedulers.append(torch.optim.lr_scheduler.StepLR(opt, steps, gamma))
 
     def get_last_lr(self):
+        return self._last_lr()
+
+    def _last_lr(self):
         last_lrs = []
         for s in self.schedulers:
             last_lrs.append(s.get_last_lr())
