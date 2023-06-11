64e6c127d7e8bb487eaa54a8053c2e95b9c87a6e
Ahmad <pouramini@gmail.com>
2023-06-11 22:22:12 +0330

    commit

--- attempt/callbacks.py
+++ attempt/callbacks.py
@@ -24,9 +24,9 @@ class PTLearningRateCallback(TrainerCallback):
             #logs["slr"] = lr._last_lr[0]
             #logs["tlr"] = lr._last_lr[1]
             #logs["step"] = state.global_step 
-            last_lrs = lr.get_last_lrs()
+            last_lrs = lr.get_last_lr()
             for i, llr in enumerate(last_lrs):
-                logs["lr" + i] = "{:.2f}".format(llr)
+                logs["lr" + str(i)] = '{:.2f}'.format('{}'.format(llr)) 
         logger.info(logs)
 
 class AnnealCallback(TrainerCallback):
