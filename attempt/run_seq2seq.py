28f71e0a1a1fd782c055e19f628ac3ff9cf16b35
Ahmad <pouramini@gmail.com>
2023-07-11 11:03:06 +0330

    commit

--- attempt/run_seq2seq.py
+++ attempt/run_seq2seq.py
@@ -1949,7 +1949,8 @@ def train(**kwargs):
                     cur_img = Image.open(img_buf)
                     #tags_img = tag_to_image(da, get_image=True)
                     #cur_img = combine_x([tags_img, cur_img])
-                    sp = op.join(str(kwargs.cat), "images") 
+                    cat = Path(kwargs.save_path).parent
+                    sp = op.join(cat, "images") 
                     Path(sp).mkdir(exist_ok=True, parents=True)
                     pic = "router_" + str(exp_info["expid"])
                     pp = sp + "/pred_" + pic + ".png"
