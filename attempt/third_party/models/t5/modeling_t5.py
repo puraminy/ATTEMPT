b59a4d3a2235fa7ea7e00fafb8efbab18092884c
Ahmad <pouramini@gmail.com>
2023-07-11 19:14:30 +0330

    commit

--- attempt/third_party/models/t5/modeling_t5.py
+++ attempt/third_party/models/t5/modeling_t5.py
@@ -1140,8 +1140,9 @@ class T5Stack(T5PreTrainedModel):
         batch_size = inputs_embeds.shape[0]
         attend_for = target_prompts
         inp_target = target_prompts
-        if self.attend_for == "ptarget": 
-            attend_for = src_prompts[:,:,-1,:,:]
+        if self.attend_for == "private": 
+            private_prompt = src_prompts[:,-1,:,:]
+            attend_for = private_prompt.unsqueeze(1)
         elif self.attend_for == "target": 
             inp_target = target_prompts
         elif self.attend_for == "inp_target": 
