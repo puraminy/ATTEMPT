3da9905bedc87171af728aea11fb34e647d7a665
Ahmad <pouramini@gmail.com>
2023-07-24 10:04:34 +0330

    commit

--- attempt/data/tasks.py
+++ attempt/data/tasks.py
@@ -436,7 +436,7 @@ class AbstractTask(abc.ABC):
             for label in targets:
                 assert label in self.labels_map, self.name + ":" + label \
                         + ":" + str(self.labels_map)
-                tt.append("<" + self.labels_map[label] + ">")
+                tt.append(self.labels_map[label])
             targets = tt 
         else:
             labels_list = self.labels_list
