e83defad3022e6a5845df43fbeecd209ec333aae
Ahmad-server <pouramini@gmail.com>
2024-06-04 18:49:11 +0330

    commit

---
+++ attempt/confs/run.sh
@@ -0,0 +1,166 @@
+alias barun="bash /home/ahmad/ATTEMPT/attempt/run.sh"
+alias abarun="bash /home/ahmad/Asai/ATTEMPT/attempt/run.sh"
+alias retrain="barun _re --do_train=True"
+alias procdf="python3 /home/ahmad/ATTEMPT/attempt/procdf.py"
+
+# Train Splitter
+
+# retrain FT -to splitter -conf splitter --t=splitter _atomic --lr=0.0003 --save=splitter --tn=500 --ep=5 _rels 
+
+# Apply Splitter
+# reval FT -conf split-omcs --t=omcs --tsn=20000 --model=t5-base-splitter-500
+
+# Apply Classifier
+# reval FT -conf classify-omcs --t=free-rels --tsn=16000 --model=t5-base-classifier-500 --cache=False 
+
+# Unsupervised Pre-Training using 8000 sample
+retrain FT --t=free-cs -to fc-2 --cache=False --tn=2000 --ep=5 --tsn=100 --bs=2 --temp=unsup#mixed#sup --lr=0.0001 --model=t5-large --save=template-free
+
+#retrain FT --t=free-cs -to fc-2 --cache=False --tn=8000 --ep=5 --tsn=100 --bs=16 --temp=unsup#mixed --lr=0.0001 --model=t5-large --save=template-free
+
+exit
+#procdf ft-map
+#procdf pt-map
+#procdf ft-filling
+#procdf pt-filling
+
+if [ -z "$1" ]; then
+   echo "Provide parameter 1"
+   exit
+fi
+
+t=$1
+for tsk in "imdb#tweet-eval"; do 
+for tune in FT; do
+for mod in base lm v1; do
+for met in  unsup mixed none sup; do # per_sample sup none; do
+for version in sent2-12000; do
+case $tsk in 
+   _*)
+          # Your code here
+       task=$tsk
+       tt="${tsk#_}"
+       echo "Variable 'task' starts with an underscore: $task"
+       ;;
+   *)
+       task="--t=$tsk"
+       tt=sp
+       ;;
+esac
+
+if [ $met = "none" ]; then
+   model="t5-$mod"
+else
+   model="t5-$mod-$met-$version" 
+fi
+echo "$model"
+if [ $t = "none" ]; then
+   folder=$tt
+else
+   folder=$tt-$t
+fi
+if [ $tune = "FT" ]; then
+   echo $task
+   retrain FT -to ft-$folder --tn=30 --d=123 $task \
+      --temp="vnat_10-v2#sup#unsup#unsup-nat" \
+      --ep=5 -skip \
+      --lr=0.0005 \
+      --model=$model \
+      --bs=4  --opt_type=ada --sph=3 --cache=True _single -merge=task_name  -last=task_name
+   exit
+else
+   retrain PT -to pt-$folder --tn=30 $task  \
+   --temp="ptar-unsup-vnat-$1#ptar-unsup-nat" \
+   --ep=15 -skip \
+   --model=$model \
+   --lr=0.1 --bs=8  \
+   --opt_type=ada --enctype=mlp_res --sph=3 \
+   --cache=True _single -merge=task_name -last=task_name 
+fi
+done 
+done
+done
+done
+done
+
+
+exit
+
+retrain FT --t=free-cs -to fc-2 --cache=False --tn=12000 --ep=3 --tsn=100 --bs=8 --temp=unsup#sup --lr=0.0001 --model=t5-v1#t5-lm#t5-base --save=template-sent2
+
+
+if [ "$1" = "sd" ]; then
+   echo 'a' | sudo -S shutdown -h now
+fi
+exit
+
+
+
+#
+#for model in v1 base lm; do
+retrain FT -to ft-clt --tn=30 _clt --temp="sup-nat#unsup-nat#sup#unsup" \
+#   --ep=5 \
+#   --lr=0.0005 \
+#   --model="t5-$model" \
+#   --bs=8  --opt_type=ada  --sph=3 --cache=True _single -merge=task_name 
+#done 
+#
+
+#for met in sup unsup per_sample; do
+#for model in v1 base lm; do
+#retrain PT -to pt-clt --tn=30 _clt  \
+#   --temp="ptar-unsup-nat#ptar-sup-nat#sup#unsup" \
+#   --ep=10 \
+#   --model=t5-$model-$met-free-8000 \
+#   --lr=0.08 --bs=4  \
+#   --opt_type=ada --enctype=mlp_res --sph=3 --cache=True _single -merge=task_name 
+#done
+#done
+
+#for met in sup unsup per_sample; do
+for model in v1 base; do
+retrain PT -to pt-clt --tn=30 _clt  \
+   --temp="ptar-unsup-nat#ptar-sup-nat#sup#unsup" \
+   --ep=10 \
+   --model=t5-$model \
+   --lr=0.1 --bs=4  \
+   --opt_type=ada --enctype=mlp_res --sph=3 --cache=True _single -merge=task_name 
+done
+#done
+
+#echo 'a' | sudo -S shutdown -h now
+exit
+--model=t5-$model-$met-free-8000 \
+
+#################
+
+for met in sup unsup; do
+for model in v1 base lm; do
+retrain FT -to ft-33 --tn=30 _pht --temp="sup-nat#unsup-nat#sup#unsup" \
+   --ep=5 \
+   --lr=0.0005 \
+   --model="t5-$model-$met-free-8000" \
+   --bs=16  --opt_type=ada  --sph=3 --cache=True _single -merge=task_name 
+done 
+done
+
+exit
+
+
+for met in per_sample; do
+for model in v1 base lm; do
+retrain FT -to ft-44 --tn=30 _ust --temp="sup-nat#unsup-nat#sup#unsup" \
+   --ep=5 \
+   --lr=0.0005 \
+   --model="t5-$model-$met-free-8000" \
+   --bs=16  --opt_type=ada  --sph=3 --cache=True _single -merge=task_name 
+done 
+done
+
+
+exit
+
+
+if [ "$1" = "sd" ]; then
+   echo 'a' | sudo -S shutdown -h now
+fi
